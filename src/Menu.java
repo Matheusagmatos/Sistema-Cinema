3b03eef941141fd3778af0413d7274f109eea8ec
MATHEUS AGUIAR MATOS <matheus1999aguiar@gmail.com>
2022-02-01 20:46:37 -0300

    Update Menu.java

--- src/Menu.java
+++ src/Menu.java
@@ -1,15 +1,52 @@
+import java.util.Scanner;
+
 public class Menu {
 
   public static void main(String[] args) {
-      
-     System.out.println("----- SISTEMA CINEMA -----");
+
+     Scanner input = new Scanner(System.in);
+
+     System.out.println("--------------------------------"); 
+     System.out.println("-------- SISTEMA CINEMA --------");
+     System.out.println("--------------------------------");
+     System.out.println();
+     System.out.println(">>> CLIENTE --------------------");
      System.out.println("( 1 ) - Nova Compra");
      System.out.println("( 2 ) - Filmes em Cartaz");
      System.out.println("( 3 ) - Informações de Compra");
-     //TEST
-     System.out.println("( 4 ) - adicionar filme");
+     System.out.println("--------------------------------");
+     System.out.println();
+     System.out.println();
+     System.out.println(">>> ADMINISTRADOR --------------");
+     System.out.println("( 4 ) - Adicionar filme");
+     System.out.println("( 5 ) - Remover Filme");
+     System.out.println("( 6 ) - Alterar Preço do Ingresso");
+     System.out.println("--------------------------------");
 
-     Filme.adicionarFilme(args);
+     int option = input.nextInt();
+     input.close();
+
+     switch(option){
+
+         case 1: 
+             Compra.novaCompra();
+
+         case 2:
+             Filme.filmesEmCartaz();
+
+         case 3:
+             Compra.infosCompra();
+
+         case 4:
+             Filme.adicionarFilme(args);
+
+         case 5:
+             Filme.removerFilme(args);
+
+         case 6:
+             Ingresso.alterarPreco();
+
+     }
      
      
 
