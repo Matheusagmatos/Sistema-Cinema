1e2ce508d6812f64da50482534ecd695d797be29
MATHEUS AGUIAR MATOS <matheus1999aguiar@gmail.com>
2022-02-06 19:36:19 -0300

    Update Administrador.java
    Criação da classe "Administrador"

--- src/Administrador.java
+++ src/Administrador.java
@@ -1,8 +1,34 @@
+import java.util.Scanner;
+
 public abstract class Administrador {
 
+    static Scanner input = new Scanner(System.in);
 
-    String nome;
-    int senha;
+    static private String user = "Administrador Geral";
+    static private int senha;
+
+    public static boolean acessoAdministrador(){
+      System.out.println(">> ACESSO DE ADMINISTRADOR NECESSÁRIO");
+      System.out.println(Administrador.user);
+      System.out.println("Digite sua senha:");
+       int senhaDigitada = input.nextInt();
+       if(senhaDigitada == senha){
+         System.out.println("Acesso permitido!");
+         return true;
+       }else{
+         System.err.println(">> Senha incorreta! Acesso negado.");  
+       }
+       return false;
+    }
+
+    public int getSenha() {
+        return senha;
+    }
+
+    public String getNome() {
+        return user;
+    }
+
     
     
 }
